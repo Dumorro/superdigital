44323b6f5272cb342ca8c0f6c4f1b25dcada2cfe
Thiago Fernandes Coelho <thiago.coelho@bhs.com.br>
2020-01-31 08:35:38 -0300

    Troca provider de cache

--- src/ContaCorrente.Extrato/ContaCorrente.Extrato.API/ConfiguracoesDeInicializacao/ConfiguracaoDoRedis.cs
+++ src/ContaCorrente.Extrato/ContaCorrente.Extrato.API/ConfiguracoesDeInicializacao/ConfiguracaoDoRedis.cs
@@ -1,7 +1,4 @@
-﻿using EasyCaching.Core;
-using EasyCaching.Core.Configurations;
-using EasyCaching.Redis;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ContaCorrente.Extrato.API.ConfiguracoesDeInicializacao
@@ -13,16 +10,16 @@ namespace ContaCorrente.Extrato.API.ConfiguracoesDeInicializacao
             var endpoint = configuration.GetSection("RedisCfg")["Endpoint"];
             var port = int.Parse(configuration.GetSection("RedisCfg")["Port"]);
             var password = configuration.GetSection("RedisCfg")["Pws"];
-            services.AddEasyCaching(cfg =>
-            {
-                cfg.UseRedis(r =>
-                {
-                    r.DBConfig.IsSsl = true;
-                    r.DBConfig.Endpoints.Add(new ServerEndPoint() {Host = endpoint, Port = port});
-                    r.DBConfig.AllowAdmin = true;
-                    r.DBConfig.Password = password;
-                }, "Redis1");
-            });
+            //services.AddEasyCaching(cfg =>
+            //{
+            //    cfg.UseRedis(r =>
+            //    {
+            //        r.DBConfig.IsSsl = true;
+            //        r.DBConfig.Endpoints.Add(new ServerEndPoint() {Host = endpoint, Port = port});
+            //        r.DBConfig.AllowAdmin = true;
+            //        r.DBConfig.Password = password;
+            //    }, "Redis1");
+            //});
         }
     }
 }
--- src/ContaCorrente.Extrato/ContaCorrente.Extrato.Infra.CacheAdaptador/CacheAdpatador.cs
+++ src/ContaCorrente.Extrato/ContaCorrente.Extrato.Infra.CacheAdaptador/CacheAdpatador.cs
@@ -2,27 +2,18 @@
 using System.Threading.Tasks;
 using ContaCorrente.Extrato.Dominio.Adaptadores;
 using ContaCorrente.Extrato.Dominio.Entidades;
-using EasyCaching.Core;
 
 namespace ContaCorrente.Extrato.Infra.CacheAdaptador
 {
     public class CacheAdaptador : ICacheAdaptador
     {
-        private readonly IEasyCachingProvider _cachingProvider;
-        private readonly IEasyCachingProviderFactory _cachingProviderFactory;
-
-        public CacheAdaptador(IEasyCachingProviderFactory cachingProviderFactory)
-        {
-            _cachingProviderFactory = cachingProviderFactory;
-            _cachingProvider = _cachingProviderFactory.GetCachingProvider("Redis1");
-        }
-
         public async Task<ExtratoDaContaCorrenteEmCache> Obter(string chave)
         {
             try
             {
-                var cache = _cachingProvider.GetAsync<ExtratoDaContaCorrenteEmCache>(chave);
-                return cache.Result.Value;
+                return null;
+                // var cache = _cachingProvider.GetAsync<ExtratoDaContaCorrenteEmCache>(chave);
+                //  return cache.Result.Value;
             }
             catch (Exception e)
             {
@@ -33,7 +24,7 @@ namespace ContaCorrente.Extrato.Infra.CacheAdaptador
 
         public async Task Adicionar(string chave, ExtratoDaContaCorrenteEmCache extrato)
         {
-            _cachingProvider.SetAsync($"cliente:{chave}", extrato, TimeSpan.FromMinutes(26000));
+            //_cachingProvider.SetAsync($"cliente:{chave}", extrato, TimeSpan.FromMinutes(26000));
         }
     }
 }
