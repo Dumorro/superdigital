04a8175d993a9c6ebbdc82fe8f0955646baffe84
Thiago Fernandes Coelho <thiago.coelho@bhs.com.br>
2020-02-09 08:50:03 -0300

    Dockerfile

--- src/ContaCorrente.Extrato/ContaCorrente.Extrato.API/ConfiguracoesDeInicializacao/ConfiguracaoDeInjecaoDeDependencia.cs
+++ src/ContaCorrente.Extrato/ContaCorrente.Extrato.API/ConfiguracoesDeInicializacao/ConfiguracaoDeInjecaoDeDependencia.cs
@@ -1,5 +1,5 @@
-﻿using System.Data;
-using System.Data.SqlClient;
+﻿    using System.Data;
+    using System.Data.SqlClient;
 using ContaCorrente.Extrato.Aplicacao.Servicos;
 using ContaCorrente.Extrato.Dominio.Adaptadores;
 using ContaCorrente.Extrato.Dominio.Servicos;
@@ -8,18 +8,19 @@ using ContaCorrente.Extrato.Infra.RedisCacheAdaptador;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ContaCorrente.Extrato.API.ConfiguracoesDeInicializacao
-{
-    public class ConfiguracaoDeInjecaoDeDependencia
+    namespace ContaCorrente.Extrato.API.ConfiguracoesDeInicializacao
     {
-        public static void Configurar(IServiceCollection services, IConfiguration configuration)
+        public class ConfiguracaoDeInjecaoDeDependencia
         {
-            services.AddSingleton(configuration);
-            services.AddScoped<IConsultaDeExtrato, ConsultaDeExtrato>();
-            services.AddScoped<IExtratoDeContaCorrente, ExtratoDeContaCorrente>();
-            services.AddScoped<ICacheAdaptador, RedisCacheAdaptador>();
-            services.AddScoped<IExtratoRepositorio, ExtratoRepositorio>();
-            services.AddScoped<IDbConnection>(d => new SqlConnection(configuration.GetConnectionString("SqlConnection")));
+            public static void Configurar(IServiceCollection services, IConfiguration configuration)
+            {
+                services.AddSingleton(configuration);
+                services.AddScoped<IConsultaDeExtrato, ConsultaDeExtrato>();
+                services.AddScoped<IExtratoDeContaCorrente, ExtratoDeContaCorrente>();
+                services.AddScoped<ICacheAdaptador, RedisCacheAdaptador>();
+                services.AddScoped<IExtratoRepositorio, ExtratoRepositorio>();
+                services.AddScoped<IDbConnection>(d =>
+                    new SqlConnection(configuration.GetConnectionString("SqlConnection")));
+            }
         }
     }
-}
